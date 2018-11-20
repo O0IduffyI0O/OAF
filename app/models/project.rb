e0297284a94a67971aff90db4b953152341313e2
Alistair Duff <alistair.g.duff@gmail.com>
2018-11-20 16:29:11 +0000

    added order routes

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,9 +3,3 @@ class Project < ActiveRecord::Base
     has_many :orders
     
 end
-
-class Order < ActiveRecord::Base
-    
-    belongs_to :project
-    
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :offers
   resources :projects, only: [:index, :show]
+  resources :orders
 end
