194efebadebf77743ea1bc48af36072b4f0b87d0
Alistair Duff <alistair.g.duff@gmail.com>
2018-11-20 17:03:30 +0000

    added order form (not working yet)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,19 @@ class OrdersController < ApplicationController
     end
     
     def create
-        
+        @order = Order.new(order_params)
+        if @order.save
+            flash[:success] = "Order Saved"
+            redirect_to root_path
+        else
+            flash[:error] = @order.errors.full_messages.join(", ")
+            redirect_to new_order_path
+        end
     end
-
+    
+    private
+    def order_params
+        params.require(:order).permit(:n_workers_male, :n_workers_female, :n_office_staff)
+    end
+    
 end
