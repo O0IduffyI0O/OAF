1a369522e8720d043cd1a8eda9f5eae27bd5ea32
Alistair Duff <alistair.g.duff@gmail.com>
2018-12-17 11:26:07 +0000

    added new project option

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,29 @@
 class ProjectsController < ApplicationController
     
+    def new
+        @project = Project.new
+    end
+    
+    def create
+        @project = Project.new(project_params)
+        if @project.save
+            redirect_to projects_path
+        else
+            flash[:error] = @project.errors.full_messages.join(", ")
+            redirect_to new_project_path
+        end
+    end
+    
     def index
     end
     
     def show
         @project = Project.find_by(id: params[:id])
     end
+    
+    private
+        def project_params
+            params.require(:project).permit(:name, :contractor, :contact)
+        end
 
 end
