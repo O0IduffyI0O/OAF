15587b14bfb1be05298dfc46c0ad7aa05f9d2a4e
Alistair Duff <alistair.g.duff@gmail.com>
2019-01-07 14:23:28 +0000

    added about page with download link for Select PDF

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def home
   end
+  
+  def about
+  end
+  
+  def download_pdf
+    send_file(
+      "#{Rails.root}/public/Select Brochure.pdf",
+      filename: "Select_Brochure.pdf",
+      type: "application/pdf"
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'pages#home'
+  get '/about', to: 'pages#about'
+  get '/about/download_pdf', to: 'pages#download_pdf'
   resources :offers
   resources :projects do
     resources :orders
